8ae63d237ef3cb5b622f6b30ef569be8752c3322
LilFreasy <ioffe.i2021@gmail.com>
2023-03-16 18:30:17 +0200

    hw6

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,14 @@ public class Main {
         Lecture lecture1 = new Lecture();
         Lecture lecture2 = new Lecture();
         Lecture lecture3 = new Lecture();
-
+        Lecture lecture4 = new Lecture();
+        Lecture lecture5 = new Lecture();
+        Lecture lecture6 = new Lecture(6, "java6");
+        int courseId = lecture6.courseId;
+        String lectureName = lecture6.lectureName;
         System.out.println("Количество лекций: " + Lecture.count);
+        System.out.println("Айди курса лекции = " + lecture6.courseId + " Название лекции: " + lecture6.lectureName);
     }
-    Lecture lecture4 = new Lecture();
-
-
-
-    Lecture lecture3 = new Lecture();
-    Lecture lecture5 = new Lecture();
 
 
 
--- src/entity/Lecture.java
+++ src/entity/Lecture.java
@@ -2,12 +2,35 @@ package entity;
 
 public class Lecture {
     public static int count;
+    public Lecture() {
+        count++;}
 
-    public Lecture(){
-        count++;
+    public int id;
+    public String name;
+
+    public int courseId;
+    public String lectureName;
+
+    public Lecture(int courseId, String lectureName) {
+        this.courseId = courseId;
+        this.lectureName = lectureName;
     }
 
-    private int id;
-    public String name;
+    public String getCourseName() {
+        return lectureName;
+    }
+
+    public void setCourseName(String CourseName) {
+        this.lectureName = CourseName;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setId(int courseId) {
+        this.courseId = courseId;
+    }
+
 
 }
