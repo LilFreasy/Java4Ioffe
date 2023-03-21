55a3da94328db0538468e46a3d05a6bd4d2ecb4c
LilFreasy <ioffe.i2021@gmail.com>
2023-03-21 19:27:13 +0200

    hw8

--- src/Main.java
+++ src/Main.java
@@ -6,56 +6,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         Lecture lecture1 = new Lecture();
         Lecture lecture2 = new Lecture();
         Lecture lecture3 = new Lecture();
-
+        Lecture lecture4 = new Lecture();
+        Lecture lecture5 = new Lecture();
+        Lecture lecture6 = new Lecture();
         System.out.println("Количество лекций: " + Lecture.count);
-    }
-
-    Lecture lecture4 = new Lecture();
-
-
-    Lecture lecture3 = new Lecture();
-    Lecture lecture5 = new Lecture();
-
-    CourseUtil courseUtil = new CourseUtil();
-
-
-     courseUtil.setUpConsole();
-    Scanner scanner = new Scanner(System.in);
-    int lk = 0;
-do{
-        System.out.println("\nChoose an option:");
-        System.out.println("1. Create a new lecture");
-        System.out.println("2. Exit");
-lk = scanner.nextInt();
-
-
+        CourseUtil courseUtil = new CourseUtil();
+        Main.setUpMenu();
+        courseUtil.setUpConsole();
 
     }
-     while (true)
+
+    private static void setUpMenu() {
         Scanner scanner = new Scanner(System.in);
-
-
-        int choice = scanner.nextInt();
-
+        int lk = 0;
+        do {
+            System.out.println("\nChoose an option:");
+            System.out.println("1. Create a new lecture");
+            System.out.println("2. Exit");
+            lk = scanner.nextInt();
+        } while (lk < 1 || lk > 2);
+        Scanner scanner1 = new Scanner(System.in);
+        int choice = scanner1.nextInt();
         if (choice == 1) {
             scanner.nextLine(); //
-
             System.out.print("Enter the lecture name: ");
             String lectureName = scanner.nextLine();
             Lecture newLecture = new Lecture();
-
-
+            newLecture.name = lectureName;
         }
+
+
+
     }
-
-
-
-
-
-
-
-
+}
